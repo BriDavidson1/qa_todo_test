81114ba16e1d181e7151c1c0cafcf0ea6094fe10
Bri Davidson <brynnd6@gmail.com>
2021-05-03 11:05:04 -0600

    todo lab

--- src/__tests__/todoTests.test.ts
+++ src/__tests__/todoTests.test.ts
@@ -1,8 +1,55 @@
-class TodoPage {}
+import {
+  Builder,
+  By,
+  Capabilities,
+  until,
+  WebDriver,
+} from "selenium-webdriver";
+const chromedriver = require("chromedriver");
+
+const driver: WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
+
+class TodoPage {
+  todoInput: By = By.className("new-todo");
+  todos: By = By.css("li.todo");
+  todoLabel: By = By.css("label");
+  todoComplete: By = By.css("input");
+  clearCompletedButton: By = By.css("button.clear-completed");
+
+  driver: WebDriver;
+  url: string = "https://devmountain.github.io/qa_todos/";
+  constructor(driver: WebDriver) {
+    this.driver = driver;
+  }
+}
+const tp = new TodoPage(driver);
 
 describe("the todo app", () => {
-  it("can add a todo", () => {});
-  it("can remove a todo", () => {});
-  it("can mark a todo with a star", () => {});
-  it("has the right number of todos listed", () => {});
-});
+  beforeEach(async () => {
+    await driver.get(tp.url);
+  });
+  afterAll(async () => {
+    await driver.quit();
+  });
+  it("Addition", async () => {
+    await driver.wait(until.elementLocated(tp.todoInput));
+    await driver.findElement(tp.todoInput).sendKeys("Walk the pupper\n");
+  });
+  it("Complete todo", async () => {
+    let myTodos = await driver.findElements(tp.todos);
+    await myTodos
+      .filter(async (todo) => {
+        (await (await todo.findElement(tp.todoLabel)).getText()) ==
+          "Walk the pupper";
+      })[0]
+      .findElement(tp.todoComplete)
+      .click();
+    await (await driver.findElement(tp.clearCompletedButton)).click();
+    myTodos = await driver.findElements(tp.todos);
+    let myTodo = await myTodos.filter(async (todo) => {
+      (await (await todo.findElement(tp.todoLabel)).getText()) == "Walk the pupper";
+    });
+    expect(myTodo.length).toEqual(0);
+  });
